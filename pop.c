db831da0643e83e825b95fc5b8a4d65347fd928b
AtoZconcept <opeyemiolaafeez@gmail.com>
2023-09-21 07:16:57 -0600

    done

--- pop.c
+++ pop.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-void pop(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number)
 {
     (void) stack;
     if (arguments->head == NULL)
